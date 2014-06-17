6a30b19f1d7c367710b2878e58020afc427db1ec
JonahFu <ferock@gmail.com>
2014-06-17 20:41:12 +0800

    添加 python 文件

--- python/temp.py
+++ python/temp.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python
+# import json
+# import urllib
+# import urllib2
+# url = "http://outofmemory.cn/code-snippet/83/sanzhong-Python-xiazai-url-save-file-code"
+# urllib.urlretrieve(url, "./1.html")
+
+
 print "Hello, world!"
