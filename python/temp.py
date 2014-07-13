e01b94339f41f2993b3d0e1ef354f02203a85ac6
JonahFu <ferock@gmail.com>
2014-07-13 22:43:41 +0800

    new py file

--- python/temp.py
+++ python/temp.py
@@ -1,9 +1,24 @@
+# coding=UTF-8  
+# file: example1.py  
+import string  
+  
+# 这个是 str 的字符串  
+s = '关关雎鸠'  
+  
+# 这个是 unicode 的字符串  
+u = u'关关雎鸠'  
+  
+print isinstance(s, str)  # True  
+print isinstance(u, unicode)  # True  
+  
+print s.__class__  # <type 'str'>  
+print u.__class__  # <type 'unicode'>
+print s
+
+
 #!/usr/bin/python
 # import json
 # import urllib
 # import urllib2
 # url = "http://outofmemory.cn/code-snippet/83/sanzhong-Python-xiazai-url-save-file-code"
 # urllib.urlretrieve(url, "./1.html")
-
-
-print "Hello, world!"
